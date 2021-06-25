573002c9a2f263ba760ac8dcb395d6ab1583e83a
Issei Horie <issei.horie@is2ei.com>
2021-06-25 21:58:23 +0900

    Change color on reloading
    Signed-off-by: Issei Horie <issei.horie@is2ei.com>

--- src/index.js
+++ src/index.js
@@ -4,20 +4,27 @@ import './style.css';
 // https://color.adobe.com/trends
 const colorSets = [
   { // 1
+    primary: '#38593d',
+    secondary: '#8c6a56',
+    tertiary: '#2d3940',
+    quaternary: '#0d0d0d',
+    textPrimary: '#dfe7f2',
+  },
+  { // 2
     primary: '#D99B84',
     secondary: '#A6916A',
     tertiary: '#D9A404',
     quaternary: '#BF7E04',
     textPrimary: '#733122',
   },
-  { // 2
+  { // 3
     primary: '#73204E',
     secondary: '#F2B6D7',
     tertiary: '#D95995',
     quaternary: '#325935',
     textPrimary: '#C9DFF2',
   },
-  { // 3
+  { // 4
     primary: '#025920',
     secondary: '#038C33',
     tertiary: '#01260E',
@@ -26,11 +33,9 @@ const colorSets = [
   }
 ]
 
-setInterval(() => {
-  const selectedColor = colorSets[Math.floor(Math.random() * colorSets.length)];
-  document.documentElement.style.setProperty('--primary-color', selectedColor.primary);
-  document.documentElement.style.setProperty('--secondary-color', selectedColor.secondary);
-  document.documentElement.style.setProperty('--tertiary-color', selectedColor.tertiary);
-  document.documentElement.style.setProperty('--quaternary-color', selectedColor.quaternary);
-  document.documentElement.style.setProperty('--text-primary-color', selectedColor.textPrimary);
-}, 3000);
+const selectedColor = colorSets[Math.floor(Math.random() * colorSets.length)];
+document.documentElement.style.setProperty('--primary-color', selectedColor.primary);
+document.documentElement.style.setProperty('--secondary-color', selectedColor.secondary);
+document.documentElement.style.setProperty('--tertiary-color', selectedColor.tertiary);
+document.documentElement.style.setProperty('--quaternary-color', selectedColor.quaternary);
+document.documentElement.style.setProperty('--text-primary-color', selectedColor.textPrimary);
